20a6d9311a3fd3bb0020dca931c983af7ae3290d
Nhu Nguyen <nhu.nguyen@buzzfeed.com>
2016-08-12 15:37:03 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+    when('/login',{
+      templateUrl: 'views/login.html',
+      controller: 'RegistrationController'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html',
+      controller:'RegistrationController'
+    }).
+    when('/success', {
+      templateUrl: 'views/success.html',
+      controller: 'SuccessController'
+    }).
+    otherwise({
+      redirectTo: '/login'
+    })
+}])
+
+myApp.controller('appController', ['$scope', function($scope){
+  $scope.message = "wellcome to my app";
+}])
